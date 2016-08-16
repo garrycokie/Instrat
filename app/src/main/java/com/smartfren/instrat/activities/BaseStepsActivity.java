7812ef484b211df7619d93ac6fb108b5beba0e69
Edwin <edwin.syarief@gmail.com>
2016-08-16 19:49:38 +0700

    templating step buttons

---
+++ app/src/main/java/com/smartfren/instrat/activities/BaseStepsActivity.java
@@ -0,0 +1,53 @@
+package com.smartfren.instrat.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.smartfren.instrat.R;
+
+/**
+ * Created by ASUS on 8/16/2016.
+ */
+public abstract class BaseStepsActivity extends AppCompatActivity {
+
+    private OnStepEventListener _stepEventListener;
+
+    protected Button _btnBack;
+    protected Button _btnContinue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        _btnBack = (Button) findViewById(R.id.btnStepBack);
+        _btnContinue = (Button) findViewById(R.id.btnStepContinue);
+
+        _btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(_stepEventListener!=null) {
+                    _stepEventListener.onBackClicked();
+                }
+            }
+        });
+        _btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(_stepEventListener != null) {
+                    _stepEventListener.onContinueClicked();
+                }
+            }
+        });
+    }
+
+    protected interface OnStepEventListener {
+        void onBackClicked();
+        void onContinueClicked();
+    }
+
+    protected void setStepEventListener(OnStepEventListener eventListener) {
+        _stepEventListener = eventListener;
+    }
+}
--- app/src/main/java/com/smartfren/instrat/activities/Block12Activity.java
+++ app/src/main/java/com/smartfren/instrat/activities/Block12Activity.java
@@ -1,15 +1,26 @@
 package com.smartfren.instrat.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.smartfren.instrat.R;
 
-public class Block12Activity extends AppCompatActivity {
+public class Block12Activity extends BaseStepsActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_block12);
+
+        super.setStepEventListener(new OnStepEventListener() {
+            @Override
+            public void onBackClicked() {
+                // TODO: button back clicked
+            }
+
+            @Override
+            public void onContinueClicked() {
+                // TODO: button continue clicked
+            }
+        });
     }
 }
